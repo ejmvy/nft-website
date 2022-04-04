0d28d3384768711cafe4298bbc217b295926e4b2
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-04-04 09:55:59 +0100

    Updates to svg roadmap animation

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -1,3 +1,6 @@
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+import { useLayoutEffect, useRef } from "react"
 import styled from "styled-components"
 import DrawSvg from "../DrawSvg"
 
@@ -46,14 +49,30 @@ const Items = styled.ul`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: lightblue;
+  
 
   &>*:nth-of-type(2n + 1) {
     justify-content: start;
+
+    div {
+      border-radius: 50px 0 50px 0;
+      text-align: right;
+    }
+    p {
+      border-radius: 40px 0 40px 0;
+    }
   }
 
   &>*:nth-of-type(2n) {
     justify-content: end;
+
+    div {
+      border-radius: 0 50px 0 50px;
+      text-align: left;
+    }
+    p {
+      border-radius: 0 40px 0 40px ;
+    }
   }
 `
 const Item = styled.li`
@@ -93,9 +112,9 @@ const Text = styled.span`
   margin: 0.5rem 0;
   `
 
-const RoadMapItem = ({ title, subText }) => {
+const RoadMapItem = ({ title, subText, addToRef }) => {
   return (
-    <Item>
+    <Item ref={addToRef}>
       <ItemContainer>
         <Box>
           <SubTitle>{title}</SubTitle>
@@ -106,6 +125,45 @@ const RoadMapItem = ({ title, subText }) => {
   )
 }
 const Roadmap = () => {
+
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+  gsap.registerPlugin(ScrollTrigger);
+
+  const addToRefs = (el) => {
+    if (el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el);
+    }
+  }
+
+  useLayoutEffect(() => {
+    let t1 = gsap.timeline();
+    revealRefs.current.forEach((el, index) => {
+
+      t1.fromTo(
+        el.childNodes[0],
+        {
+          y: '0',
+        }, {
+        y: '-30%',
+        scrollTrigger: {
+          id: `section-${index + 1}`,
+          trigger: el,
+          start: 'top center+=200px',
+          end: 'bottom center',
+          scrub: true,
+          //markers: true,
+
+        }
+      }
+      )
+    })
+
+    return () => {
+
+    };
+  }, [])
+
   return (
     <Section>
       <Title>Roadmap</Title>
@@ -116,11 +174,11 @@ const Roadmap = () => {
         </SvgContainer>
         <Items>
           <Item>&nbsp;</Item>
-          <RoadMapItem title='This is a title' subText='this is subtext' />
-          <RoadMapItem title='This is a title' subText='this is subtext' />
-          <RoadMapItem title='This is a title' subText='this is subtext' />
-          <RoadMapItem title='This is a title' subText='this is subtext' />
-          <RoadMapItem title='This is a title' subText='this is subtext' />
+          <RoadMapItem addToRef={addToRefs} title='Grand Opening' subText='Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat.' />
+          <RoadMapItem addToRef={addToRefs} title='Great Benefits' subText='Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat.' />
+          <RoadMapItem addToRef={addToRefs} title='Early Access' subText='Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat.' />
+          <RoadMapItem addToRef={addToRefs} title='New Merch' subText='Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat.' />
+          <RoadMapItem addToRef={addToRefs} title='Holders Ranking' subText='Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat.' />
         </Items>
       </Container>
     </Section>
