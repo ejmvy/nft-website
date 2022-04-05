4041167e8f674e87a45dc08c1d04bb5a154c3763
EJ McVey <ej.mcvey@geopal-solutions.com>
2022-04-05 20:13:41 +0100

    Bottom Banner above footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,139 @@
+import styled from "styled-components"
+import Banner from "./Banner"
+import Logo from './Logo'
+import Facebook from '../Icons/Facebook'
+import Instagram from '../Icons/Instagram'
+import Twitter from '../Icons/Twitter'
+import LinkedIn from '../Icons/LinkedIn'
 
+const Section = styled.section`
+min-height: 100vh;
+width: 100vw;
+background: ${props => props.theme.body};
+color: ${props => props.theme.text};
+position: relative;
+display: flex;
+flex-direction: column;
+`
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid ${props => props.theme.text};
+`
+
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const IconList = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem auto;
+
+  &>*{
+    padding-right: 0.5rem;
+    transition: all 0.2s ease;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+`
+
+const MenuItems = styled.ul`
+  list-style: none;
+  width: 50%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 1rem;
+`
+
+const Item = styled.li`
+  width: fit-content;
+  cursor: pointer;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${props => props.theme.text};
+    transition: width 0.3s ease;
+
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`
+
+const Bottom = styled.div`
+  width: 75%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    text-decoration: underline;
+  }
+`
 
 const Footer = () => {
+
+  const scrollTo = (id) => {
+    let element = document.getElementById(id);
+
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    })
+  }
+
+
   return (
-    <div>Footer</div>
+    <Section>
+      <Banner />
+
+      <Container>
+        <Left>
+          <Logo />
+          <IconList>
+            <a href='#' target='_blank' rel='noopener'><Facebook /></a>
+            <a href='#' target='_blank' rel='noopener'><Instagram /></a>
+            <a href='#' target='_blank' rel='noopener'><Twitter /></a>
+            <a href='#' target='_blank' rel='noopener'><LinkedIn /></a>
+          </IconList>
+        </Left>
+        <MenuItems>
+          <Item onClick={() => scrollTo('home')}>Home</Item>
+          <Item onClick={() => scrollTo('home')}>About</Item>
+          <Item onClick={() => scrollTo('home')}>Roadmap</Item>
+
+          <Item onClick={() => scrollTo('home')}>Showcase</Item>
+          <Item onClick={() => scrollTo('home')}>Team</Item>
+          <Item onClick={() => scrollTo('home')}>Faq</Item>
+        </MenuItems>
+      </Container>
+
+      <Bottom>
+        <span>
+          &copy; {new Date().getFullYear()} Weirdos Club. All rights reserved.
+        </span>
+        <span>
+          Made with &#10084; by <a href='http://youtube.com' target='_blank' rel='noopener noreferrer'>EJ</a>
+        </span>
+      </Bottom>
+    </Section>
   )
 }
 
